5bd88a347926d92d9dbf9e36ac4ae6e8acde5874
Husein <ahmadov.m48@gmail.com>
2023-03-01 20:41:28 +0300

    fetching comments

--- src/components/posts/PostIdPage.jsx
+++ src/components/posts/PostIdPage.jsx
@@ -8,14 +8,24 @@ const PostIdPage = () => {
   const params = useParams();
 
   const [post, setPost] = useState({});
+  const [comments, setComments] = useState([]);
   const [fetchPosts, isPostsLoading, error] = useFetching(async () => {
     const post = await PostService.getById(params.id);
 
     setPost(post.data);
   });
 
+  const [fetchComments, isCommentLoading, commentError] = useFetching(
+    async () => {
+      const comments = await PostService.getCommentsByPostId(params.id);
+
+      setComments(comments.data);
+    }
+  );
+
   useEffect(() => {
     fetchPosts();
+    fetchComments();
   }, []);
 
   if (error) {
@@ -28,8 +38,27 @@ const PostIdPage = () => {
         <Loader />
       ) : (
         <div>
-          Пост по ID: {params.id}
-          <p>{post.title}</p>
+          <h1>Пост по ID: {params.id}</h1>
+          <p style={{ fontSize: 20 }}>{post.title}</p>
+
+          <div style={{ marginTop: 50 }}>
+            <h1>Комментарии:</h1>
+            {isCommentLoading ? (
+              <Loader />
+            ) : (
+              comments.map((comment) => {
+                return (
+                  <div key={comment.id} style={{ marginTop: 30 }}>
+                    <h2>{comment.email}</h2>
+                    <p>{comment.body}</p>
+                  </div>
+                );
+              })
+            )}
+            {commentError && (
+              <div>Произошла ошибка при загрузке комментариев</div>
+            )}
+          </div>
         </div>
       )}
     </>
